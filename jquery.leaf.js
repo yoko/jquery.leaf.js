bc8d9e522a53d87019fc5172d8c2c06588b3f904
yksk <yoko@carbonfairy.org>
2009-01-16 14:28:56 +0900

    コミットミス

--- jquery.leaf.js
+++ jquery.leaf.js
@@ -1,16 +1,14 @@
 
 (function($) {
-	var uri = 'http://pipes.yahoo.com/pipes/pipe.run';
+	var uri = 'http://pipes.yahoo.com/pipes/pipe.run?_id=DM6ru9_R3BGmxGU9xAnzeQ&_render=json&_callback=?';
 	var queries = {
+		Locale        : 'jp',
 		AWSAccessKeyId: '17WV92D5WX2R1VET0H02',
-		AssociateTag  : 'clothfairy-22',
-		ItemId        : null,
-		_id           : 'DM6ru9_R3BGmxGU9xAnzeQ',
-		_render       : 'json'
+		AssociateTag  : 'clothfairy-22'
 	};
 	var asin = /ASIN\/([A-Z0-9]{10,13})/;
 	var template = $([
-		'<div class="leaf">',
+		'<div class="item">',
 		'<div class="image"/>',
 		'<div class="main">',
 		'<h5/>',
@@ -27,81 +25,103 @@
 	].join(''));
 	var container = null;
 
-	var show = function(e) {
-		var itemId = e.data.itemId;
-		var leaf = $('#leaf-'+itemId);
-		var a = $(this);
-
-		if (leaf.length)
-			leaf.show();
-		else {
-			var q = queries.extend({ ItemId: itemId }, queries);
-			$.getJSON(uri+'?_callback=?', q, function(data) {
-				var item = data.value.items[0].Items.Item;
-				var attr = item.ItemAttributes;
-				var listing = item.Offers.Offer.OfferListing;
-				var m = item.MediumImage;
-
-				var image = m ?
-					linkHTML('<img src="'+m.URL+'" alt="'+attr.Title+'" width="'+m.Width.content+'" height="'+m.Height.content+'"/>') :
-					'';
-				var date = attr.ReleaseDate || attr.PublicationDate || '-';
-				var creator = creatorHTML(attr[this]);
-				var price = (offerListing.Price.FormattedPrice || '-').replace(/￥ /, '¥');
-
-				leaf = template
-					.clone()
-					.attr('id', 'leaf-'+itemId)
-					.find('.image').html(image).end()
-					.find('h5').html('['+attr.ProductGroup.toLowerCase()+'] '+linkHTML(attr.Title)).end()
-					.find('.creator').html(creator).end()
-					.find('.price').text(price).end()
-					.find('.availability').text(offerListing.Availability || '-').end()
-					.find('.release-date').text(date).end()
-					.find('.rating').text(item.CustomerReviews.AverageRatin || '-').end()
-					.find('.asin').text(itemId).end()
-					.appendTo(container)
-					.mouseout(function(e) {
-						if ($(e.relatedTarget).parent()[0] != container[0])
-							$(this).fadeOut('fast');
-					});
-			});
-		}
-		leaf.css('top', a.offset().top + a.height() / 2 - leaf.height() / 2);
-	};
-
-	var linkHTML = function(content) {
-		return '<a href="http://www.amazon.co.jp/o/ASIN/'+item.ASIN+'/'+queries.AssociateTag+'/ref=nosim">'+content+'</a>';
+	var linkHTML = function(asin, content) {
+		return '<a href="http://www.amazon.co.jp/o/ASIN/'+asin+'/'+queries.AssociateTag+'/ref=nosim">'+content+'</a>';
 	};
 
 	var creatorHTML = function(attr) {
-		var ret = [];
+		var creators = [];
 		var type = ['Author', 'Artist', 'Manufacturer', 'Publisher'];
 		$.each(type, function() {
 			var c = attr[this];
 			if (!c) return;
 			if (typeof c == 'string') c = [c];
-			ret.push($.map(c, function(n) {
+			creators = creators.concat(
+				$.grep(c, function(n) {
+					return ($.inArray(n, creators) == -1);
+				})
+			);
+		});
+		creators = creators.length ?
+			$.map(creators, function(n) {
 				return n
 					.replace(/&/g, '&amp;')
 					.replace(/"/g, '&quot;')
 					.replace(/</g, '&lt;')
 					.replace(/>/g, '&gt;');
-			}));
-		});
-		return ret ?
-			'<li>'+$.unique(ret).join('</li><li>')+'</li>' :
-			'<li>-</li>';
+			}).join('</li><li>') :
+			'-';
+		return '<li>'+creators+'</li>';
 	};
 
-	$.fn.leaf = function() {
+	$.leafSetup = function(q) {
+		$.extend(queries, q);
+	};
+
+	$.fn.leaf = function(callback) {
 		if (!container) container = $('<div id="leaf"/>').appendTo('body');
 
 		return this.each(function() {
 			if (!this.href) return;
 			var itemId = asin.exec(this.href);
 			if (!itemId) return;
-			$(this).bind('mouseover', { itemId: itemId }, show);
+			itemId = itemId[1];
+
+			var a = $(this), position = null;
+			a.mouseover(function() {
+				var leaf = $('#leaf-'+itemId);
+
+				if (leaf.length) {
+					setPosition();
+					leaf.show();
+				}
+				else {
+					var q = $.extend({}, queries, { ItemId: itemId });
+					$.getJSON(uri, q, function(data) {
+						var items = data.value.items[0].Items;
+						if (items.Request.Errors) throw new Error(items.Request.Errors.Error.Message);
+						var item = items.Item;
+						var attr = item.ItemAttributes;
+						var listing = (item.Offers.Offer || {}).OfferListing || {};
+						var m = item.MediumImage;
+
+						var image = m ?
+							linkHTML(itemId, '<img src="'+m.URL+'" alt="'+attr.Title+'" width="'+m.Width.content+'" height="'+m.Height.content+'"/>') :
+							'';
+						var title = '['+attr.ProductGroup.toLowerCase()+'] '+linkHTML(itemId, attr.Title);
+						var creator = creatorHTML(attr);
+						var price = (listing.Price || {}).FormattedPrice || '-';
+						var availability = listing.Availability || '-';
+						var date = attr.ReleaseDate || attr.PublicationDate || '-';
+						var rating = (item.CustomerReviews || {}).AverageRating || '-';
+
+						leaf = template
+							.clone()
+							.attr('id', 'leaf-'+itemId)
+							.find('.image').html(image).end()
+							.find('h5').html(title).end()
+							.find('.creator').html(creator).end()
+							.find('.price').text(price).end()
+							.find('.availability').text(availability).end()
+							.find('.release-date').text(date).end()
+							.find('.rating').text(rating).end()
+							.find('.asin').text(itemId).end()
+							.appendTo(container)
+							.mouseout(function(e) {
+								if (!$(e.relatedTarget).parents('#leaf').length)
+									$(this).fadeOut('fast');
+							});
+							setPosition();
+
+						if (callback) callback.call(leaf);
+					});
+				}
+
+				function setPosition() {
+					position = position || a.offset().top + a.height() / 2 - leaf.height() / 2;
+					leaf.css('top', position);
+				}
+			});
 		});
 	};
 })(jQuery);
